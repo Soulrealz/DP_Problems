36f0ccfb9eabb3ba5d34a6fd3fca1a206d806111
Boris Hristov <bdhristovv@gmail.com>
2021-03-09 17:04:31 +0200

    added task about checking whether or not a sum is reachable by only adding specific given numbers

---
+++ canSum.js
@@ -0,0 +1,28 @@
+/*
+Given an array of numbers and a target
+check if that sum can be reached using only those numbers
+numbers can be repeated
+*/
+
+const canSum = (targetSum, numbers, memo = {}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return true;
+    if (targetSum < 0) return false;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        if (canSum(remainder, numbers, memo) === true) {
+            memo[targetSum] = true;
+            return true;
+        }
+    }
+
+    memo[targetSum] = false;
+    return false;
+}
+
+console.log(canSum(7, [2, 3])); // true
+console.log(canSum(301, [2, 2])); // false
+console.log(canSum(7, [2, 4])); // false
+console.log(canSum(300, [7, 14])); // false
+console.log(canSum(300, [7, 1])); // true
